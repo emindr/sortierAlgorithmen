bd9829f80f829df39db349461f6489d1bf7bb885
Emin Dervisevic <emin.dervisevic03@gmail.com>
2018-09-13 09:34:34 +0200

    Lol

--- insertionSort/src/insertionsort/InsertionSort.java
+++ insertionSort/src/insertionsort/InsertionSort.java
@@ -5,6 +5,8 @@
  */
 package insertionsort;
 
+import java.util.Random;
+
 /**
  *
  * @author EminDervisevic
@@ -15,7 +17,29 @@ public class InsertionSort {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Random r = new Random();
+        int[] arr = new int[10];
+        int temp = 0;
+        System.out.println("Not sorted:");
+            for (int i = 0; i < arr.length; i++){
+                arr[i] = r.nextInt(50)+1;
+                System.out.print(arr[i]+" ");
+            }
+        System.out.println("");
+        
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if(arr[j]<arr[j-1]){
+                    temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+        System.out.println("\nSorted:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
     }
     
 }
